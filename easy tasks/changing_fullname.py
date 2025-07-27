62248c4ce98c6093ed0d4e8018a87b3596fe3d7e
NIKITA230009 <147995617+NIKITA230009@users.noreply.github.com>
2025-07-27 12:48:16 +0300

    Update changing_fullname.py

--- easy tasks/changing_fullname.py
+++ easy tasks/changing_fullname.py
@@ -1,10 +1,2 @@
-lst = list(map(str, input().split()))
-subname = lst[2]
-patronym = lst[1]
-patronym = list(patronym)
-patronym = patronym[0]+"."
-name = lst[0]
-name = list(name)
-name = name[0]+"."
-FIO = subname+" "+name+patronym
-print(FIO)
+lst = list(map(int, input().split()))
+lst.sort()
