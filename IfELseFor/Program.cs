e2482ae369d734294b25417c1b0b23317d85e8e4
NIKITA230009 <147995617+NIKITA230009@users.noreply.github.com>
2025-09-13 18:30:55 +0300

    Add pattern matching example

---
+++ IfELseFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace IfELseFor
+{
+    class Program
+    {
+        static void Main()
+        {
+            object o = "3";
+            int j = 4;
+            if (o is int i)
+            {
+                WriteLine($"{i}X{j}={i * j}");
+            }
+            else
+            {
+                WriteLine("ERROR");
+            }
+        }
+    }
+}
